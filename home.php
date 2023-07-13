cbfa24cb398bba457ed612db791c2984631de2cd
OddScores <133257230+OddScores@users.noreply.github.com>
2023-07-13 15:55:41 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<?php include("header.php"); ?>
+	<main>
+		<section class="hero">
+			<div class="container">
+				<h1>Create.</h1>
+				<h2>We tell stories that matter.</h2>
+				<!-- Add frames from projects and the pink-eye logo here -->
+			</div>
+		</section>
+
+		<!-- Add mission statement and contact information/social media links here -->
+		<section class="mission">
+			<div class="container">
+				<h1>Our Mission</h1>
+				<p>Pink Eye Pictures is a film production company that creates meaningful and provocative films, commercials, music videos, and interviews that aim to inspire positive change and help people understand different perspectives on the human condition. Our goal is to cultivate a positive growth mindset work environment that brings in a diverse group of creatives to help us create stories that matter, and to amplify the voices of those who may not have had the chance to tell their stories before.</p>
+				<p>We believe that our work can make a difference, and we want to share our profits with our community by donating a percentage of them to local organizations that align with our values.</p>
+				<p>In addition to our creative work, we also provide commercial services for clients who need video content for their businesses or organizations. Our experienced team can help these clients develop creative concepts and bring their ideas to life through high-quality videos that align with their brand and messaging.</p>
+				<p>Overall, Pink Eye Pictures is dedicated to creating compelling content that inspires positive change and supports our community.</p>
+			</div>
+		</section>
+		<!-- Add more sections here, such as a "Featured Projects" section or a "Testimonials" section -->
+	</main>
+	<?php include("footer.php"); ?>
+</html>
